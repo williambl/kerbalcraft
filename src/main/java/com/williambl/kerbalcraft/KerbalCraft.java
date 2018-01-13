aed06bccb89827b9460698e7c415a2ba4e476d0d
williambl <william@williambl.com>
2018-01-13 19:36:38 +0000

    Fixed compilation errors

--- src/main/java/com/williambl/kerbalcraft/KerbalCraft.java
+++ src/main/java/com/williambl/kerbalcraft/KerbalCraft.java
@@ -17,7 +17,7 @@ public class KerbalCraft {
     public static final String MODID = "kerbalcraft";
     public static final String VERSION = "1.2.1";
 
-    @SidedProxy(clientSide="net.williambl.KerbalCraft.client.ClientProxy", serverSide="net.williambl.KerbalCraft.server.ServerProxy")
+    @SidedProxy(clientSide="com.williambl.kerbalcraft.client.ClientProxy", serverSide="com.williambl.kerbalcraft.server.ServerProxy")
     public static CommonProxy proxy;
 
     @EventHandler
