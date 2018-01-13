16746587ef387511274775c969270a919c5e9741
williambl <william@williambl.com>
2018-01-13 19:53:23 +0000

    Added block registering code

---
+++ src/main/java/com/williambl/kerbalcraft/common/block/ModBlocks.java
@@ -0,0 +1,72 @@
+package com.williambl.kerbalcraft.common.block;
+
+import com.williambl.kerbalcraft.KerbalCraft;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.registries.IForgeRegistry;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ModBlocks {
+
+	@Mod.EventBusSubscriber
+	public static class RegistrationHandler {
+
+		public static final Set<ItemBlock> ITEM_BLOCKS = new HashSet<>();
+
+		/**
+		 * Register this mod's {@link Block}s.
+		 *
+		 * @param event The event
+		 */
+		@SubscribeEvent
+		public static void registerBlocks(RegistryEvent.Register<Block> event) {
+			final IForgeRegistry<Block> registry = event.getRegistry();
+
+			event.getRegistry().registerAll();
+		}
+
+		/**
+		 * Register this mod's {@link ItemBlock}s.
+		 *
+		 * @param event The event
+		 */
+		@SubscribeEvent
+		public static void registerItemBlocks(RegistryEvent.Register<Item> event) {
+
+			final ItemBlock[] items = {};
+
+			final IForgeRegistry<Item> registry = event.getRegistry();
+
+			for (final ItemBlock item : items) {
+				registry.register(item.setRegistryName(item.getBlock().getRegistryName()));
+				ITEM_BLOCKS.add(item);
+			}
+		}
+
+		/**
+		 * Register this mod's ItemBlock Models.
+		 *
+		 * @param event The event
+		 */
+		@SubscribeEvent
+		public static void registerItemBlockModels(ModelRegistryEvent event) {
+		}
+
+		public static void registerTileEntities() {
+		}
+
+		private static void registerTileEntity(Class<? extends TileEntity> tileEntityClass) {
+			GameRegistry.registerTileEntity(tileEntityClass, KerbalCraft.MODID + ":" + tileEntityClass.getSimpleName().replaceFirst("TileEntity", ""));
+		}
+
+	}
+}
