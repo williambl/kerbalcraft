e902a86e53a6d3bd2cf8e664cdcb5575da3bbd40
williambl <william@williambl.com>
2018-07-11 19:31:30 +0100

    Replaced classes for all effector blocks with BlockEffector + IEffector

--- src/main/java/com/williambl/kerbalcraft/common/block/ModBlocks.java
+++ src/main/java/com/williambl/kerbalcraft/common/block/ModBlocks.java
@@ -1,12 +1,17 @@
 package com.williambl.kerbalcraft.common.block;
 
+import com.williambl.kerbalcraft.IEffector;
 import com.williambl.kerbalcraft.KerbalCraft;
+import krpc.client.RPCException;
+import krpc.client.services.SpaceCenter;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
-import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,33 +20,113 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class ModBlocks {
 
-	public static BlockStager STAGER;
-	public static BlockSASActivator SAS_ACTIVATOR;
-	public static BlockRCSActivator RCS_ACTIVATOR;
-	public static BlockGearActivator GEAR_ACTIVATOR;
-	public static BlockLightActivator LIGHT_ACTIVATOR;
-	public static BlockBrakeActivator BRAKE_ACTIVATOR;
-	public static BlockAborter ABORTER;
-	public static BlockThrottleSetter THROTTLE_SETTER;
-	public static BlockActionGroupActivator[] ACTION_GROUP_ACTIVATORS = new BlockActionGroupActivator[10];
+	public static BlockEffector STAGER;
+	public static BlockEffector SAS_ACTIVATOR;
+	public static BlockEffector RCS_ACTIVATOR;
+	public static BlockEffector GEAR_ACTIVATOR;
+	public static BlockEffector LIGHT_ACTIVATOR;
+	public static BlockEffector BRAKE_ACTIVATOR;
+	public static BlockEffector ABORTER;
+	public static BlockEffector THROTTLE_SETTER;
+	public static BlockEffector[] ACTION_GROUP_ACTIVATORS = new BlockEffector[10];
 
 	public static void AddBlocks () {
-		STAGER = new BlockStager("stager", MapColor.BLACK);
-		SAS_ACTIVATOR = new BlockSASActivator("sas_activator", MapColor.BLACK);
-		RCS_ACTIVATOR = new BlockRCSActivator("rcs_activator", MapColor.BLACK);
-		GEAR_ACTIVATOR = new BlockGearActivator("gear_activator", MapColor.BLACK);
-		LIGHT_ACTIVATOR = new BlockLightActivator("light_activator", MapColor.BLACK);
-		BRAKE_ACTIVATOR = new BlockBrakeActivator("brake_activator", MapColor.BLACK);
-		ABORTER = new BlockAborter("aborter", MapColor.BLACK);
-		THROTTLE_SETTER = new BlockThrottleSetter("throttle_setter", MapColor.BLACK);
+		STAGER = new BlockEffector("stager", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			if (power == 0) return;
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().activateNextStage();
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
+
+		SAS_ACTIVATOR = new BlockEffector("sas_activator", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().setSAS(power > 0);
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
+
+		RCS_ACTIVATOR = new BlockEffector("rcs_activator", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().setRCS(power > 0);
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
+
+		GEAR_ACTIVATOR = new BlockEffector("gear_activator", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().setGear(power > 0);
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
+
+		LIGHT_ACTIVATOR = new BlockEffector("light_activator", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().setLights(power > 0);
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
+
+		BRAKE_ACTIVATOR = new BlockEffector("brake_activator", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().setBrakes(power > 0);
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
+
+		ABORTER = new BlockEffector("aborter", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().setAbort(power > 0);
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
+
+		THROTTLE_SETTER = new BlockEffector("throttle_setter", MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+			int power = worldIn.getStrongPower(pos);
+			try {
+				SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+				vessel.getControl().setThrottle((float)power/15);
+			} catch (RPCException e) {
+				e.printStackTrace();
+			}
+		});
 
 		for (int i = 0; i < 10; i++) {
-			ACTION_GROUP_ACTIVATORS[i] = new BlockActionGroupActivator("action_group_activator_" + i, MapColor.BLACK, i);
+			final int j = i; //Sorry, I know this looks ugly, but it's needed in order for the lambda below to work
+			ACTION_GROUP_ACTIVATORS[i] = new BlockEffector("action_group_activator_" + i, MapColor.BLACK, (state, worldIn, pos, blockIn, fromPos) -> {
+				int power = worldIn.getStrongPower(pos);
+				try {
+					SpaceCenter.Vessel vessel = KerbalCraft.spaceCenter.getActiveVessel();
+					vessel.getControl().setActionGroup(j, power > 0);
+				} catch (RPCException e) {
+					e.printStackTrace();
+				}
+			});
 			System.out.println(ACTION_GROUP_ACTIVATORS[i]);
 		}
 	}
@@ -100,7 +185,7 @@ public class ModBlocks {
 				ITEM_BLOCKS.add(item);
 			}
 
-			for (BlockActionGroupActivator block : ACTION_GROUP_ACTIVATORS) {
+			for (BlockEffector block : ACTION_GROUP_ACTIVATORS) {
 				final ItemBlock item = (ItemBlock) new ItemBlock(block).setRegistryName(block.getRegistryName());
 				registry.register(item);
 				ITEM_BLOCKS.add(item);
