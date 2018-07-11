a300eda955d5be24eb09a409047acc8702a7700a
williambl <william@williambl.com>
2018-07-11 19:35:42 +0100

    Removed runRPC() from IIndicator

--- src/main/java/com/williambl/kerbalcraft/IIndicator.java
+++ src/main/java/com/williambl/kerbalcraft/IIndicator.java
@@ -1,15 +1,11 @@
 package com.williambl.kerbalcraft;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public interface IIndicator {
-
-    void runRPC(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos);
 
     int runRPCGet (IBlockState state, IBlockAccess blockAccess, BlockPos pos, EnumFacing side, boolean strongPower);
 }
